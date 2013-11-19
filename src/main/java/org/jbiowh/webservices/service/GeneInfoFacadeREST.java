6b675507d262d605a97e31c95192ab7f5a4ce6a2
r78v10a07@gmail.com <r78v10a07@gmail.com@9e235593-399b-edb4-e182-c787d5532bd4>
2013-11-19 14:24:07 +0000

    First 6.0.0 commit. Major changes on the relational schema and the JPA structure. Adding the Gene and GenePTT modules to the Webservices

--- src/main/java/org/jbiowh/webservices/service/GeneInfoFacadeREST.java
+++ src/main/java/org/jbiowh/webservices/service/GeneInfoFacadeREST.java
@@ -10,13 +10,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import org.jbiowhpersistence.datasets.gene.gene.controller.GeneInfoJpaController;
-import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2Accession;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2Ensembl;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2GenomicNucleotide;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2PMID;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2ProteinAccession;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2RNANucleotide;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2STS;
+import org.jbiowhpersistence.datasets.gene.gene.entities.Gene2UniGene;
 import org.jbiowhpersistence.datasets.gene.gene.entities.GeneGroup;
 import org.jbiowhpersistence.datasets.gene.gene.entities.GeneInfo;
 import org.jbiowhpersistence.datasets.gene.gene.entities.GeneInfoDBXrefs;
 import org.jbiowhpersistence.datasets.gene.gene.entities.GeneInfoSynonyms;
 import org.jbiowhpersistence.datasets.gene.gene.search.SearchGeneInfo;
 import org.jbiowhpersistence.datasets.gene.genome.entities.GenePTT;
+import org.jbiowhpersistence.datasets.gene.genome.entities.GeneRNT;
+import org.jbiowhpersistence.datasets.ontology.entities.Ontology;
 import org.jbiowhpersistence.utils.search.JBioWHSearch;
 
 /**
@@ -43,10 +51,166 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     }
 
     @GET
+    @Path("{id}/synonym")
+    @Produces({"application/xml", "application/json"})
+    public List<GeneInfoSynonyms> findSynonymByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGeneInfoSynonyms() != null) {
+            return new ArrayList(gene.getGeneInfoSynonyms());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/xref")
+    @Produces({"application/xml", "application/json"})
+    public List<GeneInfoDBXrefs> findXrefByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGeneInfoDBXrefs() != null) {
+            return new ArrayList(gene.getGeneInfoDBXrefs());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/group")
+    @Produces({"application/xml", "application/json"})
+    public List<GeneGroup> findGroupByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGeneGroup() != null) {
+            return new ArrayList(gene.getGeneGroup());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/proteinaccession")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2ProteinAccession> findProteinAccessionByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2ProteinAccessions() != null) {
+            return new ArrayList(gene.getGene2ProteinAccessions());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/genomicnucleotide")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2GenomicNucleotide> findGenomicNucleotideByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2GenomicNucleotides() != null) {
+            return new ArrayList(gene.getGene2GenomicNucleotides());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/rnanucleotide")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2RNANucleotide> findRNANucleotideByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2RNANucleotides() != null) {
+            return new ArrayList(gene.getGene2RNANucleotides());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/ensembl")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2Ensembl> findEnsemblByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2Ensembl() != null) {
+            return new ArrayList(gene.getGene2Ensembl());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/pmid")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2PMID> findPMIDByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2PMID() != null) {
+            return new ArrayList(gene.getGene2PMID());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/sts")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2STS> findSTSByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2STS() != null) {
+            return new ArrayList(gene.getGene2STS());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/unigene")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2UniGene> findUniGeneByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGene2UniGene() != null) {
+            return new ArrayList(gene.getGene2UniGene());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/ptt")
+    @Produces({"application/xml", "application/json"})
+    public GenePTT findPTTBID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGenePTT() != null) {
+            return gene.getGenePTT();
+        }
+        return null;
+    }
+
+    @GET
+    @Path("{id}/rnt")
+    @Produces({"application/xml", "application/json"})
+    public List<GeneRNT> findRNTByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getGenePTT() != null) {
+            return new ArrayList(gene.getGeneRNT());
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("{id}/ontology")
+    @Produces({"application/xml", "application/json"})
+    public List<Ontology> findOntologyByID(@PathParam("id") Long id) {
+        GeneInfo gene = super.find(id);
+        if (gene != null && gene.getOntology() != null) {
+            return new ArrayList(gene.getOntology());
+        }
+        return new ArrayList();
+    }
+
+    @GET
     @Path("{from}/{to}")
     @Produces({"application/xml", "application/json"})
     public List<GeneInfo> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("geneid/{id}")
+    @Produces({"application/xml", "application/json"})
+    public GeneInfo findByGeneID(@PathParam("id") Long id) {
+        parm.clear();
+        parm.put("geneID", id);
+        try {
+            return (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+        } catch (NoResultException ex) {
+        }
+        return null;
     }
 
     @GET
@@ -55,9 +219,12 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     public List<GeneInfoSynonyms> findSynonymByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGeneInfoSynonyms() != null) {
-            return new ArrayList(gene.getGeneInfoSynonyms().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGeneInfoSynonyms() != null) {
+                return new ArrayList(gene.getGeneInfoSynonyms());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
@@ -68,9 +235,12 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     public List<GeneInfoDBXrefs> findXrefByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGeneInfoDBXrefs() != null) {
-            return new ArrayList(gene.getGeneInfoDBXrefs().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGeneInfoDBXrefs() != null) {
+                return new ArrayList(gene.getGeneInfoDBXrefs());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
@@ -81,22 +251,60 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     public List<GeneGroup> findGroupByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGeneGroup() != null) {
-            return new ArrayList(gene.getGeneGroup().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGeneGroup() != null) {
+                return new ArrayList(gene.getGeneGroup());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
 
     @GET
-    @Path("geneid/{id}/accession")
+    @Path("geneid/{id}/proteinaccession")
     @Produces({"application/xml", "application/json"})
-    public List<Gene2Accession> findAccessionByGeneID(@PathParam("id") Long id) {
+    public List<Gene2ProteinAccession> findProteinAccessionByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGene2Accession() != null) {
-            return new ArrayList(gene.getGene2Accession().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2ProteinAccessions() != null) {
+                return new ArrayList(gene.getGene2ProteinAccessions());
+            }
+        } catch (NoResultException ex) {
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("geneid/{id}/genomicnucleotide")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2GenomicNucleotide> findGenomicNucleotideByGeneID(@PathParam("id") Long id) {
+        parm.clear();
+        parm.put("geneID", id);
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2GenomicNucleotides() != null) {
+                return new ArrayList(gene.getGene2GenomicNucleotides());
+            }
+        } catch (NoResultException ex) {
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("geneid/{id}/rnanucleotide")
+    @Produces({"application/xml", "application/json"})
+    public List<Gene2RNANucleotide> findRNANucleotideByGeneID(@PathParam("id") Long id) {
+        parm.clear();
+        parm.put("geneID", id);
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2RNANucleotides() != null) {
+                return new ArrayList(gene.getGene2RNANucleotides());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
@@ -104,12 +312,15 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     @GET
     @Path("geneid/{id}/ensembl")
     @Produces({"application/xml", "application/json"})
-    public List<Gene2Accession> findEnsemblByGeneID(@PathParam("id") Long id) {
+    public List<Gene2Ensembl> findEnsemblByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGene2Ensembl() != null) {
-            return new ArrayList(gene.getGene2Ensembl());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2Ensembl() != null) {
+                return new ArrayList(gene.getGene2Ensembl());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
@@ -117,12 +328,15 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     @GET
     @Path("geneid/{id}/pmid")
     @Produces({"application/xml", "application/json"})
-    public List<Gene2Accession> findPMIDByGeneID(@PathParam("id") Long id) {
+    public List<Gene2PMID> findPMIDByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGene2PMID() != null) {
-            return new ArrayList(gene.getGene2PMID().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2PMID() != null) {
+                return new ArrayList(gene.getGene2PMID());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
@@ -130,12 +344,15 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     @GET
     @Path("geneid/{id}/sts")
     @Produces({"application/xml", "application/json"})
-    public List<Gene2Accession> findSTSByGeneID(@PathParam("id") Long id) {
+    public List<Gene2STS> findSTSByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGene2STS() != null) {
-            return new ArrayList(gene.getGene2STS().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2STS() != null) {
+                return new ArrayList(gene.getGene2STS());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
@@ -143,18 +360,21 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     @GET
     @Path("geneid/{id}/unigene")
     @Produces({"application/xml", "application/json"})
-    public List<Gene2Accession> findUniGeneByGeneID(@PathParam("id") Long id) {
+    public List<Gene2UniGene> findUniGeneByGeneID(@PathParam("id") Long id) {
         parm.clear();
         parm.put("geneID", id);
-        GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
-        if (gene != null && gene.getGene2UniGene() != null) {
-            return new ArrayList(gene.getGene2UniGene().values());
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGene2UniGene() != null) {
+                return new ArrayList(gene.getGene2UniGene());
+            }
+        } catch (NoResultException ex) {
         }
         return new ArrayList();
     }
 
     @GET
-    @Path("geneid/{id}/ongenome")
+    @Path("geneid/{id}/ptt")
     @Produces({"application/xml", "application/json"})
     public GenePTT findPTTByGeneID(@PathParam("id") Long id) {
         parm.clear();
@@ -162,7 +382,6 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
         try {
             GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
             if (gene != null && gene.getGenePTT() != null) {
-                gene.getGenePTT().setGeneInfo(null);
                 return gene.getGenePTT();
             }
         } catch (NoResultException ex) {
@@ -171,12 +390,35 @@ public class GeneInfoFacadeREST extends AbstractFacade<GeneInfo> {
     }
 
     @GET
-    @Path("taxid/{id}")
+    @Path("geneid/{id}/rnt")
     @Produces({"application/xml", "application/json"})
-    public List<GeneInfo> findByTaxID(@PathParam("id") Long id) {
+    public List<GeneRNT> findRNTByGeneID(@PathParam("id") Long id) {
         parm.clear();
-        parm.put("taxID", id);
-        return ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuery("GeneInfo.findByTaxID", parm);
+        parm.put("geneID", id);
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getGenePTT() != null) {
+                return new ArrayList(gene.getGeneRNT());
+            }
+        } catch (NoResultException ex) {
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("geneid/{id}/ontology")
+    @Produces({"application/xml", "application/json"})
+    public List<Ontology> findOntologyByGeneID(@PathParam("id") Long id) {
+        parm.clear();
+        parm.put("geneID", id);
+        try {
+            GeneInfo gene = (GeneInfo) ((GeneInfoJpaController) getController(GeneInfoJpaController.class)).useNamedQuerySingleResult("GeneInfo.findByGeneID", parm);
+            if (gene != null && gene.getOntology() != null) {
+                return new ArrayList(gene.getOntology());
+            }
+        } catch (NoResultException ex) {
+        }
+        return new ArrayList();
     }
 
     @GET
--- src/main/java/org/jbiowh/webservices/service/GenePTTFacadeREST.java
+++ src/main/java/org/jbiowh/webservices/service/GenePTTFacadeREST.java
@@ -1,14 +1,19 @@
 package org.jbiowh.webservices.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import org.jbiowhpersistence.datasets.gene.gene.entities.GeneInfo;
+import org.jbiowhpersistence.datasets.gene.genebank.controller.GeneBankCDSJpaController;
 import org.jbiowhpersistence.datasets.gene.genome.controller.GenePTTJpaController;
 import org.jbiowhpersistence.datasets.gene.genome.entities.GenePTT;
+import org.jbiowhpersistence.datasets.gene.genome.search.SearchGenePTT;
 import org.jbiowhpersistence.utils.search.JBioWHSearch;
 
 /**
@@ -19,8 +24,10 @@ import org.jbiowhpersistence.utils.search.JBioWHSearch;
  * @since Nov 9, 2013
  */
 @Stateless
-@Path("genome")
+@Path("geneptt")
 public class GenePTTFacadeREST extends AbstractFacade<GenePTT> {
+
+    public final String PROTEINGI = "ProteinGi";
 
     public GenePTTFacadeREST() {
         super(GenePTT.class);
@@ -30,22 +37,93 @@ public class GenePTTFacadeREST extends AbstractFacade<GenePTT> {
     @Path("{id}")
     @Produces({"application/xml", "application/json"})
     public GenePTT find(@PathParam("id") Long id) {
-        GenePTT g = super.find(id);
-        if (g != null) {
-            g.setGeneInfo(null);
-        }
-        return g;
+        return super.find(id);
     }
 
     @GET
     @Path("{from}/{to}")
     @Produces({"application/xml", "application/json"})
     public List<GenePTT> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        List<GenePTT> gs = super.findRange(new int[]{from, to});
-        for (GenePTT g : gs) {
-            g.setGeneInfo(null);
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("{id}/geneinfo")
+    @Produces({"application/xml", "application/json"})
+    public GeneInfo findGeneInfoByProteinGi(@PathParam("id") Long id) {
+        GenePTT g = super.find(id);
+        if (g != null) {
+            return g.getGeneInfo();
         }
-        return gs;
+        return null;
+    }
+
+    @GET
+    @Path("chromosome/{id}")
+    @Produces({"application/xml", "application/json"})
+    public List<GenePTT> findGenePTTByPTTFileName(@PathParam("id") String id) {
+        parm.clear();
+        parm.put("pTTFile", id);
+        try {
+            return ((GenePTTJpaController) getController(GenePTTJpaController.class)).useNamedQuery("GenePTT.findByPTTFile", parm);
+        } catch (NoResultException ex) {
+        }
+        return new ArrayList();
+    }
+
+    @GET
+    @Path("chromosome/{id}/gene")
+    @Produces({"application/xml", "application/json"})
+    public List<GeneInfo> findGeneByPTTFileName(@PathParam("id") String id) {
+        List<GeneInfo> genes = new ArrayList();
+        parm.clear();
+        parm.put("pTTFile", id);
+        try {
+            List<GenePTT> ptts = ((GenePTTJpaController) getController(GenePTTJpaController.class)).useNamedQuery("GenePTT.findByPTTFile", parm);
+            for (GenePTT p : ptts) {
+                if (p.getGeneInfo() != null) {
+                    genes.add(p.getGeneInfo());
+                }
+            }
+        } catch (NoResultException ex) {
+        }
+        return genes;
+    }
+
+    @GET
+    @Path("chromosome/{id}/{from}/{to}/gene")
+    @Produces({"application/xml", "application/json"})
+    public List<GeneInfo> findGeneByFileNameRange(@PathParam("id") String id, @PathParam("from") Integer from, @PathParam("to") Integer to) {
+        List<GeneInfo> genes = new ArrayList();
+        parm.clear();
+        parm.put("pTTFile", id);
+        parm.put("pFrom", from);
+        parm.put("pTo", to);
+        try {
+            List<GenePTT> ptts = ((GenePTTJpaController) getController(GenePTTJpaController.class)).useNamedQuery("GenePTT.findByPFromPToPTTFile", parm);
+            for (GenePTT p : ptts) {
+                if (p.getGeneInfo() != null) {
+                    genes.add(p.getGeneInfo());
+                }
+            }
+        } catch (NoResultException ex) {
+        }
+        return genes;
+    }
+
+    @GET
+    @Path("chromosome/{id}/{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<GenePTT> findGenePTTByFileNameRange(@PathParam("id") String id, @PathParam("from") Integer from, @PathParam("to") Integer to) {
+        parm.clear();
+        parm.put("pTTFile", id);
+        parm.put("pFrom", from);
+        parm.put("pTo", to);
+        try {
+            return ((GenePTTJpaController) getController(GenePTTJpaController.class)).useNamedQuery("GenePTT.findByPFromPToPTTFile", parm);
+        } catch (NoResultException ex) {
+        }
+        return new ArrayList();
     }
 
     @GET
@@ -59,11 +137,12 @@ public class GenePTTFacadeREST extends AbstractFacade<GenePTT> {
     protected HashMap<Class, Object> createController() {
         HashMap<Class, Object> controllers = new HashMap();
         controllers.put(GenePTTJpaController.class, new GenePTTJpaController(getEntityManager().getEntityManagerFactory()));
+        controllers.put(GeneBankCDSJpaController.class, new GeneBankCDSJpaController(getEntityManager().getEntityManagerFactory()));
         return controllers;
     }
 
     @Override
     protected JBioWHSearch getSearch() {
-        return null;
+        return new SearchGenePTT();
     }
 }
